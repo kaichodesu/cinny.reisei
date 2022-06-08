1ba48dc49d828bbfb69f870bd17658512ad61d27
Krishan <33421343+kfiven@users.noreply.github.com>
2022-06-08 21:00:44 +0530

    Remove copy-tex css files as they no longer needed

--- src/app/atoms/math/Math.jsx
+++ src/app/atoms/math/Math.jsx
@@ -4,9 +4,6 @@ import PropTypes from 'prop-types';
 import katex from 'katex';
 import 'katex/dist/katex.min.css';
 
-import 'katex/dist/contrib/copy-tex';
-import 'katex/dist/contrib/copy-tex.css';
-
 const Math = React.memo(({
   content, throwOnError, errorColor, displayMode,
 }) => {
